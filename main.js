d9d15c5482506328b2917bbc620381f15be5e848
Cho Won Jae <yoteamo7@naver.com>
2021-01-04 23:59:16 +0900

    create, read 기능 알아보기

---
+++ main.js
@@ -0,0 +1,26 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+ 
+// Set some default (required if your JSON file is empty)
+db.defaults({ topic: [], author : [] })
+  .write()
+
+//Add post  
+// db.get('author')
+//     .push({ id : 1, name : "egoing", profile : "developer"})
+//     .write();
+
+// db.get('topic')
+//     .push({id : 1, title : "lowdb", description : "lowdb is ...", author : 1})
+//     .write();
+
+// db.get('topic')
+//     .push({id : 2, title : "mysql", description : "mysql is ...", author : 1})
+//     .write();
+
+console.log(db.get('topic')
+    .find({ id : 1 })
+    .value());
